7457bb9b42ee2efad90f0d8b725ca73590c42a62
luka385 <lucasbrandan26@gmail.com>
2025-04-21 12:28:42 -0300

    adapter excel soporta multiples hojas

--- projects/qh/internal/excel/adapter/excel_adapter.go
+++ projects/qh/internal/excel/adapter/excel_adapter.go
@@ -3,7 +3,6 @@ package adapter
 import (
 	"fmt"
 	"io"
-	"strconv"
 	"strings"
 
 	"github.com/devpablocristo/monorepo/projects/qh/internal/excel/handler/dto"
@@ -23,46 +22,35 @@ func (a *ExcelAdapter) ParsePersonExcel(r io.Reader) ([]dto.ExcelPerson, error) 
 	}
 	defer f.Close()
 
-	sheet := f.GetSheetName(0)
-	rows, err := f.GetRows(sheet)
-	if err != nil {
-		return nil, fmt.Errorf("Error reading rows: %w", err)
-	}
-
 	var (
 		persons []dto.ExcelPerson
 		errs    []string
 	)
 
-	for i, row := range rows {
-		if i == 0 {
-			continue
-		}
-
-		if len(row) < 4 {
-			errs = append(errs, fmt.Sprintf("incomplete %d row", i+1))
-			continue
-		}
-
-		age, err := strconv.Atoi(row[2])
+	for _, sheet := range f.GetSheetList() {
+		rows, err := f.GetRows(sheet)
 		if err != nil {
-			errs = append(errs, fmt.Sprintf("Invalid Age in row %d: %v", i+1, err))
-			continue
+			errs = append(errs, fmt.Sprintf("failed to read sheet %s: %v", sheet, err))
 		}
 
-		p := dto.ExcelPerson{
-			FirstName: row[0],
-			LastName:  row[1],
-			Age:       age,
-			Phone:     row[3],
-		}
+		for i, row := range rows {
+			if i == 0 {
+				continue
+			}
 
-		persons = append(persons, p)
+			person, err := parseRowToPerson(row)
+			if err != nil {
+				errs = append(errs, fmt.Sprintf("row %d in sheet '%s' : %v", i+1, sheet, err))
+			}
+
+			persons = append(persons, person)
+		}
 	}
 
 	if len(errs) > 0 {
-		return persons, fmt.Errorf("error in the file:\n%s", strings.Join(errs, "\n"))
+		return persons, fmt.Errorf("errors while parsing file:\n%s", strings.Join(errs, "\n"))
 	}
+
 	return persons, nil
 }
 
@@ -73,45 +61,35 @@ func (a *ExcelAdapter) ParseOrderExcel(r io.Reader) ([]dto.OrderDto, error) {
 	}
 	defer f.Close()
 
-	sheet := f.GetSheetName(0)
-	rows, err := f.GetRows(sheet)
-	if err != nil {
-		return nil, fmt.Errorf("error reading rows: %w", err)
-	}
-
 	var (
 		orders []dto.OrderDto
 		errs   []string
 	)
 
-	for i, row := range rows {
-		if i == 0 {
-			continue
-		}
-
-		if len(row) < 5 {
-			errs = append(errs, fmt.Sprintf("incomplete row %d", i+1))
-		}
-
-		total, err := strconv.ParseFloat(row[2], 64)
+	for _, sheet := range f.GetSheetList() {
+		rows, err := f.GetRows(sheet)
 		if err != nil {
-			errs = append(errs, fmt.Sprintf("invalid amount in row %d: %v", i+1, err))
+			errs = append(errs, fmt.Sprintf("failed to read sheet %s: %v", sheet, err))
 			continue
 		}
 
-		order := dto.OrderDto{
-			OrderID:     row[0],
-			Customer:    row[1],
-			TotalAmount: total,
-			Status:      row[3],
-			Date:        row[4],
-		}
+		for i, row := range rows {
 
-		orders = append(orders, order)
+			if i == 0 {
+				continue
+			}
+
+			order, err := parseRowToOrder(row)
+			if err != nil {
+				errs = append(errs, fmt.Sprintf("row %d in sheet '%s': %v", i+1, sheet, err))
+			}
+
+			orders = append(orders, order)
+		}
 	}
 
-	if len(errs) < 0 {
-		return orders, fmt.Errorf("errors in the file: \n%s", strings.Join(errs, "\n"))
+	if len(errs) > 0 {
+		return orders, fmt.Errorf("errors while parsing file:\n%s", strings.Join(errs, "\n"))
 	}
 
 	return orders, nil
